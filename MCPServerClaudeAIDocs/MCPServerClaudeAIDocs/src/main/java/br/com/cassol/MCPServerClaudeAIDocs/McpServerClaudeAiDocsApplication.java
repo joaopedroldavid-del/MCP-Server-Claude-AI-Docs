f712ed221741398caabb0d548c7f6ac79d3a5596
joaopedroldavid-del <joao.david@cassol.com.br>
2025-09-09 18:02:53 -0300

    FIRST: Commit to up the structure

---
+++ MCPServerClaudeAIDocs/MCPServerClaudeAIDocs/src/main/java/br/com/cassol/MCPServerClaudeAIDocs/McpServerClaudeAiDocsApplication.java
@@ -0,0 +1,13 @@
+package br.com.cassol.MCPServerClaudeAIDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerClaudeAiDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpServerClaudeAiDocsApplication.class, args);
+	}
+
+}
---
+++ MCPServerClaudeAIDocs/MCPServerClaudeAIDocs/src/test/java/br/com/cassol/MCPServerClaudeAIDocs/McpServerClaudeAiDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cassol.MCPServerClaudeAIDocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerClaudeAiDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
